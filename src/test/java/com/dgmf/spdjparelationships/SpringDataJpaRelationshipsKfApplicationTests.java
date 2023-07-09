f1d72d8599dae902c746fda8e6269de66c1d975a
FranckDodo91 <devlogappdgmf@gmail.com>
2023-07-09 07:33:01 +0200

    Project initialization

---
+++ src/test/java/com/dgmf/spdjparelationships/SpringDataJpaRelationshipsKfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgmf.spdjparelationships;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataJpaRelationshipsKfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
